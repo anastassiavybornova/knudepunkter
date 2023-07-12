5cb7fd564412b4e34c9e3dae6aaf319593cb5b9b
Anastassia Vybornova <anastasiavybornova@gmail.com>
2023-07-12 19:55:30 +0200

    renamed concept into beta

--- scripts/04_beta_to_network.py
+++ scripts/04_beta_to_network.py
@@ -18,7 +18,7 @@ from collections import Counter
 homepath = QgsProject.instance().homePath()
 
 # import cleaned data
-gdf = gpd.read_file(homepath + "/data/processed/workflow_steps/qgis_output_concept.gpkg")
+gdf = gpd.read_file(homepath + "/data/processed/workflow_steps/qgis_output_beta.gpkg")
 proj_crs = gdf.crs
 print("proj_crs: ", proj_crs)
 
@@ -26,7 +26,7 @@ print("proj_crs: ", proj_crs)
 G = scripts.graphedit.get_graph_from_gdf(gdf)
 
 # where to save
-filepath_to = homepath + "/data/processed/workflow_steps/G_concept.json"
+filepath_to = homepath + "/data/processed/workflow_steps/G_beta.json"
 
 # save to json
 scripts.graphedit.spatialgraph_tojson(G, proj_crs, filepath_to)
@@ -38,23 +38,23 @@ G = scripts.graphedit.spatialgraph_fromjson(filepath_to)
 
 # for plotting, save nodes and edges with component / degree information
 nodes = scripts.graphedit.get_node_gdf(G, return_degrees = True)
-mynodefile = homepath + "/data/processed/workflow_steps/nodes_concept.gpkg"
+mynodefile = homepath + "/data/processed/workflow_steps/nodes_beta.gpkg"
 nodes[["geometry", "degree"]].to_file(mynodefile, index = False)
 
 edges = scripts.graphedit.get_edge_gdf(G, return_components = True)
-myedgefile = homepath + "/data/processed/workflow_steps/edges_concept.gpkg"
+myedgefile = homepath + "/data/processed/workflow_steps/edges_beta.gpkg"
 edges[["geometry", "component_nr"]].to_file(myedgefile, index = False)
 
 # display in QGIS
 if display_layer == True:
 
-    vlayer_edges = QgsVectorLayer(myedgefile, "Edges (concept)", "ogr")
+    vlayer_edges = QgsVectorLayer(myedgefile, "Edges (beta)", "ogr")
     if not vlayer_edges.isValid():
         print("Layer failed to load!")
     else:
         QgsProject.instance().addMapLayer(vlayer_edges)
 
-    vlayer_nodes = QgsVectorLayer(mynodefile, "Nodes (concept)", "ogr")
+    vlayer_nodes = QgsVectorLayer(mynodefile, "Nodes (beta)", "ogr")
     if not vlayer_nodes.isValid():
         print("Layer failed to load!")
     else:
