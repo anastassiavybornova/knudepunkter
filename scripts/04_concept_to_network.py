d8ff2875b1d6a1844e1eb56c5e7db2cd7d50dd0a
Anastassia Vybornova <anastasiavybornova@gmail.com>
2023-07-12 19:26:40 +0200

    renamed beta into concept

--- scripts/04_concept_to_network.py
+++ scripts/04_concept_to_network.py
@@ -3,7 +3,7 @@ display_layer = True
 ### NO CHANGES BELOW THIS LINE
 
 # import python libraries
-from src import graphedit
+import scripts.graphedit
 import os
 os.environ['USE_PYGEOS'] = '0'
 import geopandas as gpd
@@ -18,43 +18,43 @@ from collections import Counter
 homepath = QgsProject.instance().homePath()
 
 # import cleaned data
-gdf = gpd.read_file(homepath + "/data/processed/workflow_steps/qgis_output_beta.gpkg")
+gdf = gpd.read_file(homepath + "/data/processed/workflow_steps/qgis_output_concept.gpkg")
 proj_crs = gdf.crs
 print("proj_crs: ", proj_crs)
 
 # make graph from data
-G = graphedit.get_graph_from_gdf(gdf)
+G = scripts.graphedit.get_graph_from_gdf(gdf)
 
 # where to save
-filepath_to = homepath + "/data/processed/workflow_steps/G_beta.json"
+filepath_to = homepath + "/data/processed/workflow_steps/G_concept.json"
 
 # save to json
-graphedit.spatialgraph_tojson(G, proj_crs, filepath_to)
+scripts.graphedit.spatialgraph_tojson(G, proj_crs, filepath_to)
 
 del(G)
 
 # import back (to check if it worked)
-G = graphedit.spatialgraph_fromjson(filepath_to)
+G = scripts.graphedit.spatialgraph_fromjson(filepath_to)
 
 # for plotting, save nodes and edges with component / degree information
-nodes = graphedit.get_node_gdf(G, return_degrees = True)
-mynodefile = homepath + "/data/processed/workflow_steps/nodes_beta.gpkg"
+nodes = scripts.graphedit.get_node_gdf(G, return_degrees = True)
+mynodefile = homepath + "/data/processed/workflow_steps/nodes_concept.gpkg"
 nodes[["geometry", "degree"]].to_file(mynodefile, index = False)
 
-edges = graphedit.get_edge_gdf(G, return_components = True)
-myedgefile = homepath + "/data/processed/workflow_steps/edges_beta.gpkg"
+edges = scripts.graphedit.get_edge_gdf(G, return_components = True)
+myedgefile = homepath + "/data/processed/workflow_steps/edges_concept.gpkg"
 edges[["geometry", "component_nr"]].to_file(myedgefile, index = False)
 
 # display in QGIS
 if display_layer == True:
 
-    vlayer_edges = QgsVectorLayer(myedgefile, "Edges (beta)", "ogr")
+    vlayer_edges = QgsVectorLayer(myedgefile, "Edges (concept)", "ogr")
     if not vlayer_edges.isValid():
         print("Layer failed to load!")
     else:
         QgsProject.instance().addMapLayer(vlayer_edges)
 
-    vlayer_nodes = QgsVectorLayer(mynodefile, "Nodes (beta)", "ogr")
+    vlayer_nodes = QgsVectorLayer(mynodefile, "Nodes (concept)", "ogr")
     if not vlayer_nodes.isValid():
         print("Layer failed to load!")
     else:
