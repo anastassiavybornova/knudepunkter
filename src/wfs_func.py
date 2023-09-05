3b1990aa5179f22324d8cd5aa5c37b8f87e8656e
Ane Rahbek Vierø <vieroe@hotmail.com>
2023-09-05 16:27:47 +0200

    added docstrings

--- src/wfs_func.py
+++ src/wfs_func.py
@@ -5,6 +5,7 @@ from qgis import processing
 
 
 def get_bounds(gdf):
+    # get bounds of geodataframe
     bounds = gdf.bounds
     minx = bounds.minx[0]
     miny = bounds.miny[0]
@@ -15,6 +16,16 @@ def get_bounds(gdf):
 
 
 def fix_geometries(input_layer):
+    """
+    Fix invalid geometries in input layer and return temporary layer with valid geoms
+
+    Arguments:
+        input_layer (vector layer): layer with (potentially) invalid geoms
+
+    Returns:
+        fixed_layer: vector layer with valid geoms
+    """
+
     fixed_layer = processing.run(
         "native:fixgeometries", {"INPUT": input_layer, "OUTPUT": "TEMPORARY_OUTPUT"}
     )["OUTPUT"]
@@ -23,6 +34,18 @@ def fix_geometries(input_layer):
 
 
 def clip_save_layer(input_layer, study_area_vlayer, filepath, layer_name):
+    """
+    Clip input layer with vector layer and save as geopackage
+
+    Arguments:
+        input_layer (vector layer): layer to be clipped
+        study_area_vlayer (vector layer): vector layer defining clip extent
+        filepath (str): filepath for saving clipped layer
+        layer_name (str): name of layer for print statement
+
+    Returns:
+        None
+    """
     clip_params = {
         "INPUT": input_layer,
         "OVERLAY": study_area_vlayer,
@@ -40,6 +63,25 @@ def clip_save_layer(input_layer, study_area_vlayer, filepath, layer_name):
 def get_wfs_layers(
     study_area_vlayer, bounds, wfs_core, wfs_name, wfs_version, homepath, proj_crs
 ):
+    """
+    - creates a new subdir for WFS connection
+    - downloads all available layers from the WFS connection
+    - clips all layers to the extent of study area
+    - saves all layers to new directory as geopackage
+
+    Arguments:
+        study_area_vlayer (vector layer): vector layer defining the study area/clip extent
+        bounds (tuple): bounds for WFS request
+        wfs_core (str): base url for WFS connection. E.g. f"https://rida-services.test.septima.dk/ows?MAP={wfs_name}&service=WFS"
+        wfs_name (str): name of WFS used to create new directory for storing data (usually same as the name used in the base WFS url)
+        wfs_version (str): version of WFS for WFS request
+        homepath (str): homepath for QGIS project
+        proj_crs (str): CRS in the format "EPSG:XXXX" used for WFS request
+
+    Returns:
+        None
+    """
+
     # define bounds
     minx, miny, maxx, maxy = bounds
 
