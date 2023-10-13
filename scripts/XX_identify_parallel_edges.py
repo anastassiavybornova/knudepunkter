7c3a2ddf03bf4d65fc5b7d7348d9fc0af2df3ea8
Ane Rahbek Vierø <vieroe@hotmail.com>
2023-10-13 11:39:54 +0200

    new version ready

--- scripts/XX_identify_parallel_edges.py
+++ scripts/XX_identify_parallel_edges.py
@@ -19,12 +19,16 @@ from qgis.core import *
 
 import geopandas as gpd
 import pandas as pd
-from shapely.geometry import Point
+from shapely.geometry import Point, LineString
 from src import graphedit
 
-output_file = homepath + "/data/processed/workflow_steps/network_parallel_edges.gpkg"
-# output_file = "../data/processed/workflow_steps/network_parallel_edges.gpkg"
-output_nodes = homepath + "/data/processed/workflow_steps/nodes_unique_id.gpkg"
+output_file_edges = (
+    homepath + "/data/processed/workflow_steps/network_parallel_edges.gpkg"
+)
+output_file_nodes = (
+    homepath + "/data/processed/workflow_steps/nodes_for_parallel_edges.gpkg"
+)
+
 
 # load data
 nodepath = (
@@ -36,10 +40,11 @@ edgepath = (
     + "/data/raw/folkersma_digital/2023-08-01 Cycling network Denmark shapefiles/stretch.shp"
 )
 
-# load data
+
+# load data (VSC version)
 # nodepath = "../data/raw/folkersma_digital/2023-08-01 Cycling network Denmark shapefiles/node.shp"
 # edgepath = "../data/raw/folkersma_digital/2023-08-01 Cycling network Denmark shapefiles/stretch.shp"
-
+# output_file = "../data/processed/workflow_steps/network_parallel_edges.gpkg"
 
 nodes = gpd.read_file(nodepath)
 edges = gpd.read_file(edgepath)
@@ -47,46 +52,71 @@ edges = gpd.read_file(edgepath)
 nodes.to_crs("EPSG:25832", inplace=True)
 edges.to_crs("EPSG:25832", inplace=True)
 
-# create unique id
-edges["edge_id"] = edges.index
-nodes["node_id"] = nodes.index
+# create distinct column names for unique id
+edges["edge_id"] = edges.id
+nodes["node_id"] = nodes.id
 
-nodes.to_file(output_nodes)
+# assign edges initial start and end nodes
+edges = graphedit.assign_edges_start_end_nodes(edges, nodes)
 
-# Extract start and end coordinates of each linestring
-first_coord = edges["geometry"].apply(lambda g: Point(g.coords[0]))
-last_coord = edges["geometry"].apply(lambda g: Point(g.coords[-1]))
+# find all child nodes with parents that are not dead ends
+child_nodes = nodes[(nodes.refmain.notna()) & (nodes.deadend == 0)]
 
-# Add start and end as columns to the gdf
-edges["start_coord"] = first_coord
-edges["end_coord"] = last_coord
+edges["modified"] = False
 
-start_coords = edges[["edge_id", "start_coord"]].copy()
-start_coords.set_geometry("start_coord", inplace=True, crs=edges.crs)
+# assign edges from child nodes with parents to parent nodes
+for ix, row in child_nodes.iterrows():
+    # ID of this child node
+    this_node_id = row.node_id
 
-end_coords = edges[["edge_id", "end_coord"]].copy()
-end_coords.set_geometry("end_coord", inplace=True, crs=edges.crs)
+    # geometry of the child nodes parent node
+    parent_geom = nodes.loc[
+        nodes.node_id == int(child_nodes.loc[ix, "refmain"])
+    ].geometry.values[0]
 
-# join start and end coors to nearest node
-start_joined = start_coords.sjoin_nearest(
-    nodes[["geometry", "node_id"]], how="left", distance_col="distance"
-)
-end_joined = end_coords.sjoin_nearest(
-    nodes[["geometry", "node_id"]], how="left", distance_col="distance"
-)
+    # all edges which have this child node as their start node
+    edges_start = edges.loc[edges.u == this_node_id]
 
-edges.drop(["start_coord", "end_coord"], axis=1, inplace=True)
+    # all edges which have this child node as their end node
+    edges_end = edges.loc[edges.v == this_node_id]
 
-# Merge with edges
-edges = edges.merge(
-    start_joined[["edge_id", "node_id"]], left_on="edge_id", right_on="edge_id"
-)
-edges.rename({"node_id": "u"}, inplace=True, axis=1)
+    for ix, row in edges_start.iterrows():
+        # get coordinate in edge linestring
+        edge_coords = list(row.geometry.coords)
 
-edges = edges.merge(
-    end_joined[["edge_id", "node_id"]], left_on="edge_id", right_on="edge_id"
-)
-edges.rename({"node_id": "v"}, inplace=True, axis=1)
+        # replace start coordinate (child node) with geometry of parent node
+        edge_coords[0] = parent_geom.coords[0]
+
+        # create new linestring from updated coordinates
+        new_linestring = LineString(edge_coords)
+
+        # update edge geometry
+        edges.loc[ix, "geometry"] = new_linestring
+
+        # mark edge as modified
+        edges.loc[ix, "modified"] = True
+
+    for ix, row in edges_end.iterrows():
+        # get coordinate in edge linestring
+        edge_coords = list(row.geometry.coords)
+
+        # replace end coordinate (child node) with geometry of parent node
+        edge_coords[-1] = parent_geom.coords[0]
+
+        # create new linestring from updated coordinates
+        new_linestring = LineString(edge_coords)
+
+        # update edge geometry
+        edges.loc[ix, "geometry"] = new_linestring
+
+        # mark edge as modified
+        edges.loc[ix, "modified"] = True
+
+# drop old u,v columns
+edges.drop(["u", "v"], axis=1, inplace=True)
+
+# find new start and end nodes
+edges = graphedit.assign_edges_start_end_nodes(edges, nodes)
 
 # find edges with same start and end node (but could still be on different roads!)
 edges["key"] = 0
@@ -96,33 +126,45 @@ graphedit.order_edge_nodes(edges)
 
 edges = graphedit.find_parallel_edges(edges)
 
-child_node_ids = nodes.loc[nodes.ismain == 0]["node_id"].to_list()
+# child_node_ids = nodes.loc[nodes.ismain == 0]["node_id"].to_list()
 
+# # Identify parallel edges on same roads that should be excluded from e.g. length computation
+# # Based on parent-child node classification
+# parallel_edges = []
+# grouped_edges = edges.groupby(["u", "v"])
+# assert len(grouped_edges) == len(edges.loc[edges.key == 0])
 
-# Identify parallel edges on same roads that should be excluded from e.g. length computation
-# Based on parent-child node classification
-parallel_edges = []
-grouped_edges = edges.groupby(["u", "v"])
-assert len(grouped_edges) == len(edges.loc[edges.key == 0])
+# for nodes_ids, group in grouped_edges:
+#     if (
+#         nodes_ids[0] in child_node_ids
+#         and nodes_ids[1] in child_node_ids
+#         and len(group) > 1
+#     ):
+#         parallel_edges.append(group["length"].idxmin())
 
-for nodes_ids, group in grouped_edges:
-    if (
-        nodes_ids[0] in child_node_ids
-        and nodes_ids[1] in child_node_ids
-        and len(group) > 1
-    ):
-        parallel_edges.append(group["length"].idxmin())
+# edges["parallel"] = 0
+# edges.loc[parallel_edges, "parallel"] = 1
 
-edges["parallel"] = 0
-edges.loc[parallel_edges, "parallel"] = 1
+# assert len(edges.loc[edges.parallel == 1]) == len(parallel_edges)
 
-assert len(edges.loc[edges.parallel == 1]) == len(parallel_edges)
+# delete modified edges whith same u and v
+edges.drop(
+    edges.loc[(edges.u == edges.v) & (edges.modified == True)].index, inplace=True
+)
+
+nodes_in_use_ids = set(edges.u.to_list() + edges.v.to_list())
+nodes_in_use = nodes.loc[nodes.node_id.isin(nodes_in_use_ids)]
+
+assert len(nodes.loc[nodes.ismain == True]) == len(
+    nodes_in_use.loc[nodes_in_use.ismain == True]
+)
 
 # Export
-edges.to_file(output_file)
+nodes_in_use.to_file(output_file_nodes)
+edges.to_file(output_file_edges)
 
 # Remove temporary layers from project if they exist already
-remove_existing_layers(["input edges", "input nodes", "parallel edges"])
+remove_existing_layers(["input edges", "input nodes", "parallel edges", "output nodes"])
 
 if display_input:
     # input data
@@ -130,7 +172,7 @@ if display_input:
     QgsProject.instance().addMapLayer(input_edges)
     draw_recent_simple_line_layer(color="purple", width=0.5)
 
-    input_nodes = QgsVectorLayer(output_nodes, "input nodes", "ogr")
+    input_nodes = QgsVectorLayer(nodepath, "input nodes", "ogr")
     QgsProject.instance().addMapLayer(input_nodes)
 
     draw_categorical_layer(
@@ -138,27 +180,36 @@ if display_input:
         "ismain",
         outline_width=0.0,
         alpha=200,
-        marker_size=4,
+        marker_size=2,
     )
 
     zoom_to_layer("input data")
 
 if display_output:
-    key_edges = QgsVectorLayer(
-        output_file, "parallel edges (start and end nodes)", "ogr"
-    )
-    QgsProject.instance().addMapLayer(key_edges)
-
-    draw_categorical_layer(
-        "parallel edges (start and end nodes)", "key", alpha=180, line_style="dash"
-    )
-
-    parallel_edges = QgsVectorLayer(output_file, "parallel edges (same road)", "ogr")
+    parallel_edges = QgsVectorLayer(output_file_edges, "parallel edges", "ogr")
     QgsProject.instance().addMapLayer(parallel_edges)
 
+    draw_categorical_layer("parallel edges", "key", alpha=180, line_style="solid")
+
+    # parallel_edges = QgsVectorLayer(output_file, "parallel edges (same road)", "ogr")
+    # QgsProject.instance().addMapLayer(parallel_edges)
+
+    # draw_categorical_layer(
+    #     "parallel edges (same road)", "parallel", alpha=180, line_style="solid"
+    # )
+
+    output_nodes = QgsVectorLayer(output_file_nodes, "output nodes", "ogr")
+    QgsProject.instance().addMapLayer(output_nodes)
+
     draw_categorical_layer(
-        "parallel edges (same road)", "parallel", alpha=180, line_style="solid"
+        "output nodes",
+        "ismain",
+        outline_width=0.0,
+        alpha=200,
+        marker_size=4,
     )
+
+    zoom_to_layer("parallel edges")
 
 if display_input and display_output:
     group_layers(
@@ -166,8 +217,8 @@ if display_input and display_output:
         [
             "input edges",
             "input nodes",
-            "parallel edges (start and end nodes)",
-            "parallel edges (same road)",
+            "output nodes",
+            "parallel edges",
         ],
         remove_group_if_exists=True,
     )
@@ -175,10 +226,7 @@ if display_input and display_output:
 if display_input == False and display_output == True:
     group_layers(
         "Find parallel edges",
-        [
-            "parallel edges (start and end nodes)",
-            "parallel edges (same road)",
-        ],
+        ["parallel edges", "output nodes"],
         remove_group_if_exists=True,
     )
 
